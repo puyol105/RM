fe6db35a26077146c38437b1f971c7e6a3f299d9
Renato Cruzinha <renatocruzinha@hotmail.com>
2022-04-24 18:12:11 +0100

    Add files via upload

---
+++ event.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 2;
+    while(i<=100){
+        printf("\n");
+        printf("Events%d.class = MessageEventGenerator\n",i);
+        printf("Events%d.interval = 60\n",i);
+        printf("Events%d.size = 10k\n",i);
+        printf("Events%d.hosts = %d,%d\n",i,i-1,i-1);
+        printf("Events%d.tohosts = 100,250\n",i);
+        printf("Events%d.prefix = M%d\n",i,i);
+        i++;
+    }
+    return 0;
+}
+
+/*
+Events2.class = MessageEventGenerator
+Events2.interval = 60
+Events2.size = 10k
+Events2.hosts = 1,1
+Events2.tohosts = 100,205
+Events2.prefix = M2
+*/
---
+++ sensor.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand(time(0));
+    int largura = 2500;
+    int altura = 2100;
+    int i = 2;
+    while(i<=100){
+        int r1 = randomNumber(700,largura);
+        int r2 = randomNumber(500,altura);
+        printf("\n");
+        printf("Group%d.groupID = s\n",i);
+        printf("Group%d.nrofHosts=1\n",i);
+        printf("Group%d.movementModel = StationaryMovement\n",i);
+        printf("Group%d.nodeLocation = %d,%d\n",i,r1,r2);
+        printf("Group%d.routeFile = data/Braga.wkt\n",i);
+        printf("Group%d.routeType = 1\n",i);
+        printf("Group%d.nrofInterfaces = 1\n",i);
+        printf("Group%d.interface1 = btInterface\n",i);
+        i++;
+    }
+    return 0;
+}
+
+int randomNumber(int lower, int upper) 
+{ 
+    int num = (rand() % (upper - lower + 1)) + lower; 
+    return num;
+} 
+
+/*
+Group2.groupID = s
+Group2.nrofHosts=1
+Group2.movementModel = StationaryMovement
+Group2.nodeLocation = 0,0
+Group2.routeFile = data/Braga.wkt
+Group2.routeType = 1
+Group2.nrofInterfaces = 1
+Group2.interface1 = btInterface
+*/
